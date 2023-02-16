41fe276e7c7618989f265a7c156d602b09d29ab3
Juan Rios <juanrios@tuta.io>
2023-02-15 23:50:32 -0500

    Redefined folder structure to include key software in individual folders

--- config/zsh/aliases.zsh
+++
@@ -1,3 +0,0 @@
-# Aliases
-alias notebook='conda activate notebook && cd "/mnt/d/Documents/Programming languages/Rust REPL" && jupyter-lab'
-alias vim='nvim'
---
+++ zsh/aliases.zsh
@@ -0,0 +1,3 @@
+# Aliases
+alias notebook='conda activate notebook && cd "/home/overcode/Things/Documents/Programming languages/Rust REPL" && jupyter-lab'
+alias vim='nvim'
