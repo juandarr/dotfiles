4d65a6d8fa1917579153b46bbd2e0422fe273704
Juan Rios <juanrios@tuta.io>
2023-02-18 01:27:12 -0500

    Added: new alias to open bpytop with top; alacritty window now has no title bar; new tmux configuration file to manage sessions in tmux

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,8 @@
 # Aliases
 alias notebook='conda activate notebook && cd "/home/overcode/Things/Documents/Programming languages/Rust REPL" && jupyter-lab'
 alias vim='nvim'
+alias top='bpytop'
+
+# Access to parent folder
 alias ..='cd ../'
 alias ...='cd ../../'
