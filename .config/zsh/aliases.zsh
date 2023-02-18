35fae8fb65dc113db0174b21de4f8b53d5b17bef
Juan Rios <juanrios@tuta.io>
2023-02-18 00:00:12 -0500

    Added two new aliases to go to the parent directory one and two levels up in the directory tree

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,3 +1,5 @@
 # Aliases
 alias notebook='conda activate notebook && cd "/home/overcode/Things/Documents/Programming languages/Rust REPL" && jupyter-lab'
 alias vim='nvim'
+alias ..='cd ../'
+alias ...='cd ../../'
